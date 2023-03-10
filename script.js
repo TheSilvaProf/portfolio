e07f9825de2633d6e436637d9fdb4bde699789be
TheSilvaProf <fabio01@protonmail.ch>
2023-03-09 21:27:24 -0300

    Mais uma vez sem sucesso

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".muliple-text", {
+    strings: ["Frontend Developer", "YouTuber", "Blogger"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
