8cdea2cad2ba6eb5c1e85cd2708d23618cc1cf3c
TheSilvaProf <fabio01@protonmail.ch>
2023-03-12 09:00:30 -0300

    Portfolio funcionando

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed("#typed", {
-    strings: ['Hello there!', 'How are you?'],
+    strings: ['Physical Education Teacher', 'Technology Enthusiast', 'DEV after 40'],
     typeSpeed: 35,
     startDelay: 500,
     backDelay: 1000,
